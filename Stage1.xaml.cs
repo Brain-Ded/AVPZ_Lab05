ca0850ff8f68db7a549eb05b2dfd0779a7e0f099
Brain-Ded <72191534+Brain-Ded@users.noreply.github.com>
2022-11-07 22:23:10 +0200

    Mocha

--- Stage1.xaml.cs
+++ Stage1.xaml.cs
@@ -121,6 +121,139 @@ namespace AVPZ_Lab05
             AllRiskPercent.Content = (Convert.ToInt32(TechSum.Content) + Convert.ToInt32(CostSum.Content)
                                    + Convert.ToInt32(PlanSum.Content) + Convert.ToInt32(RealSum.Content)) / Max2 * 100;
 
+            //Stage2
+            TechValue1.Content = TechLabel1.SelectedIndex;
+            TechValue2.Content = TechLabel2.SelectedIndex;
+            TechValue3.Content = TechLabel3.SelectedIndex;
+            TechValue4.Content = TechLabel4.SelectedIndex;
+            TechValue5.Content = TechLabel5.SelectedIndex;
+            TechValue6.Content = TechLabel6.SelectedIndex;
+            TechValue7.Content = TechLabel7.SelectedIndex;
+            TechValue8.Content = TechLabel8.SelectedIndex;
+            TechValue9.Content = TechLabel9.SelectedIndex;
+            TechValue10.Content = TechLabel10.SelectedIndex;
+            TechValue11.Content = TechLabel11.SelectedIndex;
+
+            R0C3Label.Content = rand.NextDouble();
+            R0C4Label.Content = rand.NextDouble();
+            R0C5Label.Content = rand.NextDouble();
+            R0C6Label.Content = rand.NextDouble();
+            R0C7Label.Content = rand.NextDouble();
+            R0C8Label.Content = rand.NextDouble();
+            R0C9Label.Content = rand.NextDouble();
+            R0C10Label.Content = rand.NextDouble();
+            R0C11Label.Content = rand.NextDouble();
+            R0C12Label.Content = rand.NextDouble();
+
+            R1C3Label.Content = rand.NextDouble();
+            R1C4Label.Content = rand.NextDouble();
+            R1C5Label.Content = rand.NextDouble();
+            R1C6Label.Content = rand.NextDouble();
+            R1C7Label.Content = rand.NextDouble();
+            R1C8Label.Content = rand.NextDouble();
+            R1C9Label.Content = rand.NextDouble();
+            R1C10Label.Content = rand.NextDouble();
+            R1C11Label.Content = rand.NextDouble();
+            R1C12Label.Content = rand.NextDouble();
+
+            R2C3Label.Content = rand.NextDouble();
+            R2C4Label.Content = rand.NextDouble();
+            R2C5Label.Content = rand.NextDouble();
+            R2C6Label.Content = rand.NextDouble();
+            R2C7Label.Content = rand.NextDouble();
+            R2C8Label.Content = rand.NextDouble();
+            R2C9Label.Content = rand.NextDouble();
+            R2C10Label.Content = rand.NextDouble();
+            R2C11Label.Content = rand.NextDouble();
+            R2C12Label.Content = rand.NextDouble();
+
+            R3C3Label.Content = rand.NextDouble();
+            R3C4Label.Content = rand.NextDouble();
+            R3C5Label.Content = rand.NextDouble();
+            R3C6Label.Content = rand.NextDouble();
+            R3C7Label.Content = rand.NextDouble();
+            R3C8Label.Content = rand.NextDouble();
+            R3C9Label.Content = rand.NextDouble();
+            R3C10Label.Content = rand.NextDouble();
+            R3C11Label.Content = rand.NextDouble();
+            R3C12Label.Content = rand.NextDouble();
+
+            R4C3Label.Content = rand.NextDouble();
+            R4C4Label.Content = rand.NextDouble();
+            R4C5Label.Content = rand.NextDouble();
+            R4C6Label.Content = rand.NextDouble();
+            R4C7Label.Content = rand.NextDouble();
+            R4C8Label.Content = rand.NextDouble();
+            R4C9Label.Content = rand.NextDouble();
+            R4C10Label.Content = rand.NextDouble();
+            R4C11Label.Content = rand.NextDouble();
+            R4C12Label.Content = rand.NextDouble();
+
+            R5C3Label.Content = rand.NextDouble();
+            R5C4Label.Content = rand.NextDouble();
+            R5C5Label.Content = rand.NextDouble();
+            R5C6Label.Content = rand.NextDouble();
+            R5C7Label.Content = rand.NextDouble();
+            R5C8Label.Content = rand.NextDouble();
+            R5C9Label.Content = rand.NextDouble();
+            R5C10Label.Content = rand.NextDouble();
+            R5C11Label.Content = rand.NextDouble();
+            R5C12Label.Content = rand.NextDouble();
+
+            R6C3Label.Content = rand.NextDouble();
+            R6C4Label.Content = rand.NextDouble();
+            R6C5Label.Content = rand.NextDouble();
+            R6C6Label.Content = rand.NextDouble();
+            R6C7Label.Content = rand.NextDouble();
+            R6C8Label.Content = rand.NextDouble();
+            R6C9Label.Content = rand.NextDouble();
+            R6C10Label.Content = rand.NextDouble();
+            R6C11Label.Content = rand.NextDouble();
+            R6C12Label.Content = rand.NextDouble();
+
+            R7C3Label.Content = rand.NextDouble();
+            R7C4Label.Content = rand.NextDouble();
+            R7C5Label.Content = rand.NextDouble();
+            R7C6Label.Content = rand.NextDouble();
+            R7C7Label.Content = rand.NextDouble();
+            R7C8Label.Content = rand.NextDouble();
+            R7C9Label.Content = rand.NextDouble();
+            R7C10Label.Content = rand.NextDouble();
+            R7C11Label.Content = rand.NextDouble();
+            R7C12Label.Content = rand.NextDouble();
+
+            R8C3Label.Content = rand.NextDouble();
+            R8C4Label.Content = rand.NextDouble();
+            R8C5Label.Content = rand.NextDouble();
+            R8C6Label.Content = rand.NextDouble();
+            R8C7Label.Content = rand.NextDouble();
+            R8C8Label.Content = rand.NextDouble();
+            R8C9Label.Content = rand.NextDouble();
+            R8C10Label.Content = rand.NextDouble();
+            R8C11Label.Content = rand.NextDouble();
+            R8C12Label.Content = rand.NextDouble();
+
+            R9C3Label.Content = rand.NextDouble();
+            R9C4Label.Content = rand.NextDouble();
+            R9C5Label.Content = rand.NextDouble();
+            R9C6Label.Content = rand.NextDouble();
+            R9C7Label.Content = rand.NextDouble();
+            R9C8Label.Content = rand.NextDouble();
+            R9C9Label.Content = rand.NextDouble();
+            R9C10Label.Content = rand.NextDouble();
+            R9C11Label.Content = rand.NextDouble();
+            R9C12Label.Content = rand.NextDouble();
+
+            R10C3Label.Content = rand.NextDouble();
+            R10C4Label.Content = rand.NextDouble();
+            R10C5Label.Content = rand.NextDouble();
+            R10C6Label.Content = rand.NextDouble();
+            R10C7Label.Content = rand.NextDouble();
+            R10C8Label.Content = rand.NextDouble();
+            R10C9Label.Content = rand.NextDouble();
+            R10C10Label.Content = rand.NextDouble();
+            R10C11Label.Content = rand.NextDouble();
+            R10C12Label.Content = rand.NextDouble();
 
             //Stage3
             PathOfFight0.SelectedIndex = rand.Next(0, 4);
@@ -245,6 +378,19 @@ namespace AVPZ_Lab05
 
                 AllRiskPercent.Content = (Convert.ToInt32(TechSum.Content) + Convert.ToInt32(CostSum.Content)
                                        + Convert.ToInt32(PlanSum.Content) + Convert.ToInt32(RealSum.Content)) / Max2 * 100;
+
+                //Stage2
+                TechValue1.Content = TechLabel1.SelectedIndex;
+                TechValue2.Content = TechLabel2.SelectedIndex;
+                TechValue3.Content = TechLabel3.SelectedIndex;
+                TechValue4.Content = TechLabel4.SelectedIndex;
+                TechValue5.Content = TechLabel5.SelectedIndex;
+                TechValue6.Content = TechLabel6.SelectedIndex;
+                TechValue7.Content = TechLabel7.SelectedIndex;
+                TechValue8.Content = TechLabel8.SelectedIndex;
+                TechValue9.Content = TechLabel9.SelectedIndex;
+                TechValue10.Content = TechLabel10.SelectedIndex;
+                TechValue11.Content = TechLabel11.SelectedIndex;
             }
         }
     }
